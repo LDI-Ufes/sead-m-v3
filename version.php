be7a6a9e7e8c4d1b0cf26e54ae3b7d77dc27848a
natodp <renato714@outlook.com>
2018-09-06 08:24:03 -0300

    Fechamento da versão 2018090500
    Ajuste de escrita do version.php e rodapé híbrido.

--- version.php
+++ version.php
@@ -8,8 +8,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-$plugin->version = '2018090500';
-$plugin->requires = '2018041200';
+defined('MOODLE_INTERNAL') || die;
+$plugin->version = 2018090500;
+$plugin->requires = 2018041200;
 $plugin->component = 'theme_sead';
-$plugin->dependencies = [ 'theme_boost' => '2017111300' ];
+$plugin->dependencies = array(
+    'theme_boost' => 2017111300,
+);
